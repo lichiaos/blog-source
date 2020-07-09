03e257bdc11cb058a93a9ba2cc66a3dfc1646e71
DustSmile <18693268439@163.com>
2020-07-09 08:51:14 +0800

    设置session

--- app.js
+++ app.js
@@ -1,65 +1,93 @@
 const querystring = require('querystring')
-const hadnlerBlogRouter = require('./src/router/blog')
 const hadnlerUserRouter = require('./src/router/user')
 const handlerBlogRouter = require('./src/router/blog')
-const {
-  resolve
-} = require('path')
-const {
-  rejects
-} = require('assert')
-
 let getPostData = ((req) => {
-  return new Promise((resolve, rejects) => {
-    if (req.method !== 'POST') {
-      resolve({})
-      return
-    }
-    if (req.headers['content-type'] !== 'application/json') {
-      resolve({})
-      return
-    }
-    let postData = ''
-    req.on('data', chunk => {
-      postData += chunk.toString()
+    return new Promise((resolve, rejects) => {
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(JSON.parse(postData))
+        })
     })
-    req.on('end', () => {
-      if (!postData) {
-        resolve({})
-        return
-      }
-      resolve(JSON.parse(postData))
-    })
-  })
 })
 
+let getCookieExpires = () => {
+    let date = new Date()
+    date.setTime(date.getTime() + 24 * 3600 * 1000)
+    return date.toGMTString()
+}
+
+const SESSION_DATA = {}
+
 const serverHandler = (req, res) => {
-  res.setHeader('Content-type', 'application/json')
-  const url = req.url
-  req.path = url.split('?')[0]
-  req.query = querystring.parse(url.split('?')[1])
+    res.setHeader('Content-type', 'application/json')
+    const url = req.url
+    req.path = url.split('?')[0]
+    req.query = querystring.parse(url.split('?')[1])
+    req.cookie = {}
 
+    // 解析cookie
+    const cookieStr = req.headers.cookie || ''
+    cookieStr.split(';').forEach(item => {
+        if (item) {
+            const [key, value] = item.split('=')
+            req.cookie[key] = value
+        }
+    })
 
-  getPostData(req).then(postData => {
-    req.body = postData
-    let blogData = handlerBlogRouter(req, res)
-    if (blogData) {
-        blogData.then(blogData => {
-           res.end(JSON.stringify(blogData))
-        }) 
-        return
+    // 解析session
+    let needSetCookie = false
+    let userId = req.cookie.userId
+
+    if (!SESSION_DATA[userId]) SESSION_DATA[userId] = {}
+    if (!userId) {
+        userId = `${Date.now()}_${Math.random()}`
+        needSetCookie = true
     }
-    let userData = hadnlerUserRouter(req, res)
-    if (userData) {
-      res.end(JSON.stringify(userData))
-      return
-    }
-      res.writeHead(404, {
-    'Content-type': 'text/plain'
-      })
-      res.write('404 Not Found')
-      res.end()
-  })
+    req.session =  SESSION_DATA[userId]
+    
+    getPostData(req).then(postData => {
+        req.body = postData
+        let blogData = handlerBlogRouter(req, res)
+        if (blogData) {
+            blogData.then(blogData => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
+                res.end(JSON.stringify(blogData))
+            })
+            return
+        }
+        let userData = hadnlerUserRouter(req, res)
+        if (userData) {
+            userData.then(blogData => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
+                res.end(JSON.stringify(blogData))
+            })
+            return
+        }
+        res.writeHead(404, {
+            'Content-type': 'text/plain'
+        })
+        res.write('404 Not Found')
+        res.end()
+    })
 }
 
 module.exports = serverHandler
--- src/router/user.js
+++ src/router/user.js
@@ -1,13 +1,21 @@
-const handlerBlogRouter = require("./blog")
-
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 const handlerUserRouter = ((req, res) => {
-  const method = req.method
-  
-  if (method === 'POST' && req.path === '/api/user/login') {
-    return {
-      msg: '这是登录接口'
+    const method = req.method
+    const {
+        username,
+        password
+    } = req.body
+
+    if (method === 'POST' && req.path === '/api/user/login') {
+       let result = loginCheck(username, password)
+      return  result.then(loginData => {
+           if (loginData.username) {
+               return new SuccessModel(loginData)
+           }
+           return new ErrorModel('登录失败')
+       })
     }
-  }
 })
 
-module.exports = handlerBlogRouter
+module.exports = handlerUserRouter
